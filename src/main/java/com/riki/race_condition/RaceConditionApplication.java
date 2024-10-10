d847387f72ccb805c73e71d6aa8e18b605f48037
n0tx <rcandra91@msn.com>
2024-10-10 20:19:39 +0700

    inisialisasi data rekening baru dan nilai saldo awal

--- src/main/java/com/riki/race_condition/RaceConditionApplication.java
+++ src/main/java/com/riki/race_condition/RaceConditionApplication.java
@@ -1,13 +1,34 @@
 package com.riki.race_condition;
 
+import com.riki.race_condition.model.Rekening;
+import com.riki.race_condition.repository.RekeningRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RaceConditionApplication {
+public class RaceConditionApplication  implements CommandLineRunner {
+
+	@Autowired
+	RekeningRepository rekeningRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(RaceConditionApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		if (rekeningRepository.count() == 0) {
+			// Membuat rekening dan menambahkan saldo
+			Rekening rekening = rekeningRepository.save(new Rekening(1L, 1000));
+			System.out.println("Initial data rekening baru berhasil dibuat");
+			System.out.println("rekening ID: " + rekening.getId());
+			System.out.println("Saldo: " + rekening.getSaldo());
+		} else {
+			System.out.println("Data sudah ada di tabel rekening");
+		}
+
+	}
 }
